03b9335e8f76a275ff8b33a44224d0d1602574d2
Karol Sean <ksean@oa-l4.21-school.ru>
2019-11-01 19:33:41 +0300

    Finished GNL

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,47 +6,53 @@
 /*   By: ksean <ksean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 18:31:15 by ksean             #+#    #+#             */
-/*   Updated: 2019/10/25 21:44:29 by ksean            ###   ########.fr       */
+/*   Updated: 2019/11/01 18:49:00 by ksean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_makestr(char *str)
+static	char	*ft_makestr(char **str)
 {
 	char	*s;
+	char	*s2;
+	char	*out;
 	int		i;
 
+	out = *str;
 	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
+	while (out[i] != '\n' && out[i] != '\0')
 		i++;
-	s = ft_strsub(str, 0, i);
-	return (s);
-}
-
-char	*ft_sub(char *str)
-{
-	char	*s;
-	int		i;
-
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	if (str[i] == '\n')
-		s = ft_strsub(str, (i + 1), ft_strlen(&(str[i + 1])));
+	s = ft_strsub(out, 0, i);
+	if (out[i] == '\n')
+	{
+		s2 = ft_strsub(out, (i + 1), ft_strlen(&(out[i + 1])));
+		free(*str);
+		*str = s2;
+	}
 	else
-		return (NULL);
+	{
+		free(*str);
+		*str = NULL;
+	}
 	return (s);
 }
 
-int		get_next_line(int fd, char **line)
+static	int		ft_return(int ret, char **out)
+{
+	free(*out);
+	*out = NULL;
+	return ((ret < 0) ? -1 : 0);
+}
+
+int				get_next_line(int fd, char **line)
 {
 	int				ret;
 	char			buf[BUFF_SIZE + 1];
-	static char		*out[10240];
+	static char		*out[OPEN_MAX];
 	char			*tmp;
 
-	if (fd < 0)
+	if (fd < 0 || fd > OPEN_MAX)
 		return (-1);
 	if (out[fd] == NULL)
 		out[fd] = ft_strnew(0);
@@ -59,17 +65,10 @@ int		get_next_line(int fd, char **line)
 		if (ft_strchr(buf, '\n') != NULL)
 			break ;
 	}
-	if (ret < 0)
-		return (-1);
 	if (ret > 0 || ft_strlen(out[fd]))
 	{
-		*line = ft_makestr(out[fd]);
-		tmp = ft_sub(out[fd]);
-		free((void*)out[fd]);
-		out[fd] = tmp;
+		*line = ft_makestr(&out[fd]);
 		return (1);
 	}
-	free(out[fd]);
-	out[fd] = NULL;
-	return (0);
+	return (ft_return(ret, &out[fd]));
 }
--- Get_Next_Line/main.c
+++ Get_Next_Line/main.c
@@ -6,7 +6,7 @@
 /*   By: ksean <ksean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 20:51:08 by ksean             #+#    #+#             */
-/*   Updated: 2019/10/25 21:25:53 by ksean            ###   ########.fr       */
+/*   Updated: 2019/11/01 18:34:05 by ksean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,66 +16,28 @@ int main(void)
 {
 	char *line;
 	int fd1;
-	int fd2 = 0;
-	//int a;
+	int fd2;
 
-	//line = ft_strnew(28348);
-	if((fd1 = open("gnl7_2.txt", O_RDONLY)) == -1)
+	if ((fd1 = open("OUT", O_RDONLY)) == -1)
 		{
 			ft_putstr("open fail\n");
 			return 1;
 		}
-	// if((fd2 = open("itoa", O_RDONLY)) == -1)
-	// 	{
-	// 		ft_putstr("open fail\n");
-	// 		return 1;
-	// 	}
-	// int fd3 = -1;
- 	while((get_next_line(fd1, &line)) > 0)
+	if ((fd2 = open("itoa", O_RDONLY)) == -1)
 		{
-			fd2++;
+			ft_putstr("open fail\n");
+			return 1;
+		}
+ 	while ((get_next_line(fd1, &line)) > 0)
+		{
 			ft_putendl(line);
 			free(line);
-
 		}
-	ft_putnbr(fd2);
-	// while((get_next_line(fd2, &line)) != 0)
-	// 	{
-	// 		ft_putendl(line);
-	// 		free(line);
-	// 	}
-
-	// get_next_line(fd1, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd2, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd1, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd1, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd1, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd2, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd2, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd2, &line);
-	// 	ft_putendl(line);
-	// get_next_line(-3, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// get_next_line(fd, &line);
-	// 	ft_putendl(line);
-	// printf("%i", a);
+	while ((get_next_line(fd2, &line)) != 0)
+		{
+			ft_putendl(line);
+			free(line);
+		}
 	close(fd1);
 	close(fd2);
 	return 0;
