5cf732ee7319bbd115ba06797cb1119343d3c716
Karol Sean <ksean@oa-j4.21-school.ru>
2019-11-02 17:17:23 +0300

    Final project for evaluation, deleted some garbage

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: ksean <ksean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 11:48:50 by ksean             #+#    #+#             */
-/*   Updated: 2019/11/01 19:31:22 by ksean            ###   ########.fr       */
+/*   Updated: 2019/11/01 18:44:05 by ksean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 85000
+# define BUFF_SIZE 99
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
--- Get_Next_Line/main.c
+++ Get_Next_Line/main.c
@@ -6,39 +6,31 @@
 /*   By: ksean <ksean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 20:51:08 by ksean             #+#    #+#             */
-/*   Updated: 2019/11/01 18:34:05 by ksean            ###   ########.fr       */
+/*   Updated: 2019/11/01 23:23:58 by ksean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
 	char *line;
 	int fd1;
-	int fd2;
 
-	if ((fd1 = open("OUT", O_RDONLY)) == -1)
+	if (argc == 2)
+	{
+		if ((fd1 = open(argv[1], O_RDONLY)) == -1)
 		{
 			ft_putstr("open fail\n");
 			return 1;
 		}
-	if ((fd2 = open("itoa", O_RDONLY)) == -1)
-		{
-			ft_putstr("open fail\n");
-			return 1;
-		}
- 	while ((get_next_line(fd1, &line)) > 0)
-		{
-			ft_putendl(line);
-			free(line);
-		}
-	while ((get_next_line(fd2, &line)) != 0)
-		{
-			ft_putendl(line);
-			free(line);
-		}
-	close(fd1);
-	close(fd2);
+
+		while ((get_next_line(fd1, &line)) > 0)
+			{
+				ft_putendl(line);
+				free(line);
+			}
+		close(fd1);
+	}
 	return 0;
 }
